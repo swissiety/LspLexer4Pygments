131891efcca90c532eeee645184c3e07061d6b95
Markus Schmidt <markus262@web.de>
2022-01-06 04:29:56 +0100

    setup config

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
-
 setup (
-  name='LspLexer',
+  name='LspLexer4Pygments',
+  version='0.1.0',
+  description='Connect a Language Server Protocol (LSP) Server with Pygments via a lexer implementation that is fed by the SemanticTokens Request',
+  long_description='',
+  url='https://github.com/swissiety/LspLexer4Pygments',
+  keywords='pygments,mkdocs',
+  author='Markus Schmidt',
+  author_email='markus262@web.de',
+
+  python_requires='>=3.8',
+  install_requires=[
+    'Pygments>=2.11.1',
+    'pylspclient>=0.0.2'
+  ],
+
   packages=find_packages(),
-  entry_points =
+  entry_points=
   """
   [pygments.lexers]
   lsplexer = lsplexer.lexer:LspLexer
